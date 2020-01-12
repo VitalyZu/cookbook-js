66bcd5c61d6dd84efcd45a319ec965f60c69034b
Vitaly <vitaly.z.vz@gmail.com>
2020-01-12 16:30:42 +0300

    'init-v2'

--- app/app.module.js
+++ app/app.module.js
@@ -1,12 +1,24 @@
 'use strict';
-angular.module('cookBook', ['categoriesList'])
+angular.module('cookBook', ['categoriesList', 'ngRoute'])
   .constant('_', window._)
-  .run(function ($http, $rootScope) {
+  .run( function ($http, $rootScope, $timeout) {
     $rootScope.isLoaded = false
     $http.get('http://127.0.0.1:8887/base.json')
       .then(response => {
-        $rootScope.data = response.data
-        $rootScope.isLoaded = true
+        setTimeout(function(){
+
+          $timeout(function() { // anything you want can go here and will safely be run on the next digest.
+            $rootScope.data = response.data
+            $rootScope.isLoaded = true
+            $rootScope.$broadcast('tag',response.data)
+          })
+
+        },2000)
+        
+        
       })
   })
+  
+  
+  
 
--- app/categories-list/categories-list.component.js
+++ app/categories-list/categories-list.component.js
@@ -1,13 +1,24 @@
 angular.module('categoriesList')
     .component('categoriesList', {
         templateUrl: 'categories-list/categories-list.template.html',
-        controller: ['$scope', '$rootScope','$route', function CategoriesListController($scope, $rootScope,$route) {
-            this.arr = [1,2,3]
-            setTimeout(()=>{
-                
-                this.arr.push(4)
-                $route.reload()},1000)
-            setTimeout(()=>console.log(this.arr),2000)
+        controller: ['$scope', '$rootScope', '$route', '$timeout', function CategoriesListController($scope, $rootScope, $route, $timeout) {
+            $scope.isLoaded = $rootScope.isLoaded
+            console.log($rootScope)
+            $timeout(function () { // anything you want can go here and will safely be run on the next digest.
+                $scope.$apply(function () {
+                    // Обворачиваем функцией $apply
+                    $scope.data = $rootScope.data;
+                    console.log($scope.data)
+                });
+            })
+            $scope.$on('tag',function(e,v){
+                console.log(v)
+                $scope.base = v 
+                console.log($scope)
+            })
+
+            console.log($scope)
+
         }
         ]
     })
