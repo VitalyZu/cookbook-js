34a093c03e57d06df939e3a985f8cfaabc041a24
Зуев Виталий Александрович <vitaly.z.vz@gmail.com>
2020-01-15 16:26:59 +0300

    'pings'

--- app/categories-list/categories-list.component.js
+++ app/categories-list/categories-list.component.js
@@ -1,23 +1,25 @@
 angular.module('categoriesList')
     .component('categoriesList', {
         templateUrl: 'categories-list/categories-list.template.html',
-        controller: ['$scope', '$rootScope', '$timeout', 'httpservice', function CategoriesListController($scope, $rootScope, $timeout, httpservice) {
+        controller: ['$scope', '$rootScope', '$timeout', 'httpservice',`$filter`, function CategoriesListController($scope, $rootScope, $timeout, httpservice,$filter) {
             var self = this
             //$rootScope.categoryID = 0;
             let promise = httpservice
             promise.then(function (resp) {
                 $scope.categories = resp.data.categories
                 $scope.recipes = resp.data.recipes
+                $rootScope.a = ($filter('filter')($scope.recipes, $rootScope.searchValue)).length
             })
             $scope.changeCategory = function (id, index) {
                 $rootScope.categoryID = id
                 $rootScope.page = 1
                 $rootScope.categoryActive.name = index;
                 $rootScope.pageActive.page = 1
-                let a = $rootScope.searchValue
-                $rootScope.searchValue = a
-                console.log($rootScope.searchValue)
+                /* let a = $rootScope.searchValue
+                $rootScope.searchValue = a */
             }
+
+            
 
         }
         ]
--- app/recipes-list/recipes-list.component.js
+++ app/recipes-list/recipes-list.component.js
@@ -1,7 +1,7 @@
 angular.module('recipesList')
     .component('recipesList', {
         templateUrl: 'recipes-list/recipes-list.template.html',
-        controller: ['$scope', '$rootScope', '$timeout', 'httpservice', function RecipesListController($scope, $rootScope, $timeout, httpservice) {
+        controller: ['$scope', '$rootScope', '$timeout', 'httpservice','$filter', function RecipesListController($scope, $rootScope, $timeout, httpservice,$filter) {
             var self = this
             $scope._ = _
             $scope.isLoaded = $rootScope.isLoaded
@@ -26,9 +26,16 @@ angular.module('recipesList')
                 })
             })
             $scope.handleInput = function (e) {
-                console.log(e)
                 $rootScope.searchValue = e
                 $rootScope.page = 1
+                let a = $rootScope.a
+                if(!(a===($filter('filter')($scope.recipes, $rootScope.searchValue)).length)){
+                    $rootScope.a = ($filter('filter')($scope.recipes, $rootScope.searchValue)).length
+                    let elem = document.getElementsByClassName('badge')[0]
+                    elem.style.backgroundColor = 'white'
+                    setTimeout(()=>elem.style.backgroundColor = '#ffc107')
+                }
+                
             }
 
         }
